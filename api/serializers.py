557d6286f34ede8e93ef9940f904c8d6f6aa1114
lamas1901 <ilhanchanel20002@gmail.com>
2022-11-13 07:13:17 +0300

    fix

--- api/serializers.py
+++ api/serializers.py
@@ -11,6 +11,7 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = (
+            'title',
             'link',
             'link_github',
             'image',
